2ca76bd2b88457f39278d19bb23c3eb887105661
wanggang <890321wang>
2021-05-31 10:06:40 +0800

    添加服务网关

---
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -0,0 +1,22 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description TODO
+ * @Author wanggang
+ * @Date 2021年05月31日
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LyApiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class, args);
+    }
+
+}
