88cf051e78432fa78f66e7156f31f1afac17a7e5
wanggang <890321wang>
2021-05-31 13:47:26 +0800

    品牌的查询功能，异步、分页、查询条件搜索

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,12 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Description TODO
+ * @Author wanggang
+ * @Date 2021年05月31日
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
