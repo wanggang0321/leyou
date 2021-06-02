253ad2aacc637110b39c01404f02e1012e7c1b24
wanggang <890321wang>
2021-06-02 20:47:11 +0800

    1.搭建FastDFS服务器(192.168.8.82)，CentOS7 + FastDFS-v5.08 2.修改图片上传功能，保存到FastDFS服务器上

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,14 +1,19 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.controller.UploadController;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +28,10 @@ public class UploadService {
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
     // 支持的文件类型
-    private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
+    private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg", "image/jpg");
+
+    @Autowired
+    FastFileStorageClient storageClient;
 
     public String upload(MultipartFile file) {
         try {
@@ -40,21 +48,54 @@ public class UploadService {
                 logger.info("上传失败，文件内容不符合要求");
                 return null;
             }
-            // 2、保存图片
-            // 2.1、生成保存目录
-            File dir = new File("D:\\heima\\upload");
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            // 2.2、保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
 
-            // 2.3、拼接图片地址
-            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
+            // 保存文件到本地路径
+            //String url = uploadToLocal(file);
+            String url = uploadToFastDFS(file);
 
             return url;
         } catch (Exception e) {
             return null;
         }
     }
+
+    /**
+     * 将图片上传到FastDFS
+     * @param file
+     * @return
+     */
+    private String uploadToFastDFS(MultipartFile file) throws IOException {
+
+        // 获取文件后缀名
+        String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+
+        // 上传
+        StorePath storePath = storageClient.uploadFile(
+                file.getInputStream(), file.getSize(), extension, null);
+
+        // 返回完整路径
+        String url = "http://image.leyou.com/" + storePath.getFullPath();
+
+        return url;
+    }
+
+    /**
+     * 保存文件到本次磁盘路径
+     */
+    private String uploadToLocal(MultipartFile file) throws IOException {
+        // 2、保存图片
+        // 2.1、生成保存目录
+        File dir = new File("D:\\heima\\upload");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        // 2.2、保存图片
+        file.transferTo(new File(dir, file.getOriginalFilename()));
+
+        // 2.3、拼接图片地址
+        String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
+
+        return url;
+    }
+
 }
--- ly-upload/src/test/java/com/leyou/FdfsTest.java
+++ ly-upload/src/test/java/com/leyou/FdfsTest.java
@@ -30,10 +30,10 @@ public class FdfsTest {
 
     @Test
     public void testUpload() throws FileNotFoundException {
-        File file = new File("D:\\test\\baby.png");
+        File file = new File("D:\\test\\2.jpg");
         // 上传并且生成缩略图
         StorePath storePath = this.storageClient.uploadFile(
-                new FileInputStream(file), file.length(), "png", null);
+                new FileInputStream(file), file.length(), "jpg", null);
         // 带分组的路径
         System.out.println(storePath.getFullPath());
         // 不带分组的路径
@@ -42,10 +42,10 @@ public class FdfsTest {
 
     @Test
     public void testUploadAndCreateThumb() throws FileNotFoundException {
-        File file = new File("D:\\heima\\upload\\caise.png");
+        File file = new File("D:\\test\\2.jpg");
         // 上传并且生成缩略图
         StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
-                new FileInputStream(file), file.length(), "png", null);
+                new FileInputStream(file), file.length(), "jpg", null);
         // 带分组的路径
         System.out.println(storePath.getFullPath());
         // 不带分组的路径
